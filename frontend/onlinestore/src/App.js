0c0c25c150852c08483daf2f461461373e035e07
Parise MohanKumar <mohankumar.parise523@gmail.com>
2024-07-07 17:51:48 +0530

    axios using

--- frontend/onlinestore/src/App.js
+++ frontend/onlinestore/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	let api = axios.create({
+		baseURL:'http://localhost:8080/api'
+	})
+	useEffect(
+		()=> {
+			getProducts()
+		},[]
+	)
+	async function getProducts() {
+		let res = await api.get('/products')
+		
+		updateProducts(res.data)
+		console.log(res.data)
+	}
 }
 
 export default App;
